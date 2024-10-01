1401d092244994c946121bb7a054a231dfd5897e
Artur <alimayko91@gmail.com>
2024-10-01 23:07:34 +0300

    add isActive Navlink

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,28 @@
 import React from 'react';
-import styles from "./components/Site.module.css";
+import s from "./components/Site.module.css";
 import {PageOne} from "./components/pages/PageOne";
 import {PageThree} from "./components/pages/PageThree";
 import {PageTwo} from "./components/pages/PageTwo";
 import {Navigate, NavLink, Route, Routes} from "react-router-dom";
 import {Error404} from "./components/pages/Error404";
 
-
 function App() {
     return (
         <div>
-            <div className={styles.header}><h1>HEADER</h1></div>
-            <div className={styles.body}>
-                <div className={styles.nav}>
-                    <div><NavLink to={'/page1'}>Page 1</NavLink></div>
-                    <div><NavLink to={'/page2'}>Page 2</NavLink></div>
-                    <div><NavLink to={'/page3'}>Page 3</NavLink></div>
+            <div className={s.header}><h1>HEADER</h1></div>
+            <div className={s.body}>
+                <div className={s.nav}>
+                    <div><NavLink to={'/page1'} className={({isActive}) => isActive ? s.active : s.navLink}>Page
+                        1</NavLink>
+                    </div>
+                    <div><NavLink to={'/page2'} className={({isActive}) => isActive ? s.active : s.navLink}>Page
+                        2</NavLink>
+                    </div>
+                    <div><NavLink to={'/page3'} className={({isActive}) => isActive ? s.active : s.navLink}>Page
+                        3</NavLink>
+                    </div>
                 </div>
-                <div className={styles.content}>
+                <div className={s.content}>
                     <Routes>
                         <Route path="/page1" element={<PageOne/>}/>
                         <Route path="/" element={<Navigate to={'/page1'}/>}/>
@@ -30,7 +35,7 @@ function App() {
                     </Routes>
                 </div>
             </div>
-            <div className={styles.footer}> abibas 2023</div>
+            <div className={s.footer}> abibas 2023</div>
         </div>
     );
 }
