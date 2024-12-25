a949c6f1ecb07c18101a7427e022c41421ce0d83
ArturLimaiko <alimayko91@gmail.com>
2024-12-25 15:29:45 +0300

    lesson 10 - Переход с синтаксиса 6.0-6.2 на 6.4. #2

--- src/components/pages/Adidas.tsx
+++ src/components/pages/Adidas.tsx
@@ -42,7 +42,6 @@ export const Adidas = () => {
     return (
         <div>
             <h2> ADIDAS</h2>
-
             <div style={{
                 display: 'flex',
                 flexDirection: 'row',
--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -3,6 +3,9 @@ import {createBrowserRouter,} from "react-router-dom";
 import App from "../App";
 import {Error404} from "../components/pages/Error404";
 import {Adidas} from "../components/pages/Adidas";
+import {Puma} from "../components/pages/Puma";
+import {Abibas} from "../components/pages/Abibas";
+import {Prices} from "../components/pages/Prices";
 
 const PATH = {
     ADIDAS: '/adidas',
@@ -10,19 +13,34 @@ const PATH = {
     ABIBAS: '/abibas',
     PRICES: '/prices',
     ERROR404: '/error404',
-}
+} as const
 
 export const router = createBrowserRouter([
     {
         path: "/",
         element: <App/>, // то что лежит в корне
-        errorElement: <Error404/> ,//обработка ошибки
+        errorElement: <Error404/>,//обработка ошибки
         children: [ // тут все наши Route
             {
                 path: PATH.ADIDAS,
                 element: <Adidas/>,
             },
-
+            {
+                path: PATH.PUMA,
+                element: <Puma/>
+            },
+            {
+                path: PATH.ABIBAS,
+                element: <Abibas/>
+            },
+            {
+                path: PATH.PRICES,
+                element: <Prices/>
+            },
+            {
+                path: PATH.ERROR404,
+                element: <Error404/>
+            }
         ]
     }
 ]);
